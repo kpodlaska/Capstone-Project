6ed1ef1e59035970b15bc0ba292904c79618037b
kpodlaska <kpodlaska@griddynamics.com>
2022-06-07 10:42:33 +0200

    path.py updated

--- path.py
+++ path.py
@@ -9,5 +9,7 @@ import ast
 f_name = "test.json"
 d_schema = "{\"date\": \"timestamp:\",\"name\": \"str:rand\",\"type\": \"['client', 'partner', 'government']\",\"age\": \"int:rand(1, 90)\"}"
 schema_dict=ast.literal_eval(d_schema.strip())
+schema_dict2=json.loads(d_schema)
 
 print(type(schema_dict), schema_dict.keys(), schema_dict.values())
+print(type(schema_dict2), schema_dict2.keys(), schema_dict2.values())
