662077fce4b20b8839c516d95bb062ff0467eeea
kpodlaska <kpodlaska@griddynamics.com>
2022-07-04 10:02:59 +0200

    some refactoring

--- main.py
+++ main.py
@@ -94,15 +94,6 @@ def create_list_of_fake_data(schema):
             elif "rand" in possibility:
                 value = creating_name_from_numbers_and_lowercase()
                 result.append(value)
-                """
-            elif 'rand' not in possibility and "str:" in possibility:
-                naked_possibility = possibility.replace("str:", "")
-                if naked_possibility.isalpha():
-                    result.append(naked_possibility)
-                else:
-                    logging.error(
-                        "You choose str type value for non str deflaut parameter! Value MagicIsCool  was used instead")
-                    result.append("MagicIsCool")"""
 
     return result
 
@@ -119,16 +110,17 @@ def init_arg_parser():
     config = configparser.ConfigParser()
     config.read("default.ini")
     parser.add_argument('--path_to_save_files',
-                        help="Define a path to output file. For current path use '.', As deflaut is ./Capstone",
+                        help="Define a path to output file. For current path use '.', As deflaut is ./Users/kpodlaska/PycharmProjects/pythonProject/Capstone",
                         type=str,
                         default=config["DEFAULT"]["path_to_save_files"])
-    parser.add_argument('--files_count', help="Define how many json file do you want to generate", type=int, default=config["DEFAULT"]["files_count"])
-    parser.add_argument('--file_name', help="Choose name to your file", type=str, default=config["DEFAULT"]["file_name"])
-    parser.add_argument('--file_prefix', help="If you chose more than 1 output file please choose prefix", type=str,
+    parser.add_argument('--files_count', help="Define how many json file do you want to generate. Default value is 1", type=int, default=config["DEFAULT"]["files_count"])
+    parser.add_argument('--file_name', help="Choose name to your file. Default value is fake_data_file", type=str, default=config["DEFAULT"]["file_name"])
+    parser.add_argument('--file_prefix', help="If you chose more than 1 output file please choose prefix. Default is count", type=str,
                         choices=['count', 'random', 'uuid'], default=config["DEFAULT"]["file_prefix"])
-    parser.add_argument('--data_schema', help='Provide data Schema for your output files', default=config["DEFAULT"]["data_schema"])
-    parser.add_argument('--data_lines', help='How many lines your output file has', default=config["DEFAULT"]["data_lines"])
-    parser.add_argument('--multiprocessing', help='The number of processes used to create files',default=config["DEFAULT"]["multiprocessing"] )
+    parser.add_argument('--data_schema', help='Provide data Schema for your output files. Default is data_schema = {"date": "timestamp:","name": "str:rand","type": "["client", "partner", "government"]","animal_type": "["cat", "dog", "monkey,"tiger"]","age": "int:rand(1, 90)","kids_number": "int:rand(1, 6)"}',
+ default=config["DEFAULT"]["data_schema"])
+    parser.add_argument('--data_lines', help='How many lines your output file has. Default is 100', default=config["DEFAULT"]["data_lines"])
+    parser.add_argument('--multiprocessing', help='The number of processes used to create files. default is 5',default=config["DEFAULT"]["multiprocessing"] )
     parser.add_argument('--clear_path', help='Clear all files in path_to_save_files that match file_name', action='store_true')
     args = parser.parse_args()
 
@@ -231,6 +223,7 @@ def main():
     init_logger()
     parsed_args = init_arg_parser()
     logging.info(parsed_args)
+    number_of_processes = os.cpu_count()
 
     logging.info(f'Current path : {parsed_args.path_to_save_files}')
     logging.info(f'You choose to generate {parsed_args.files_count} files')
@@ -248,14 +241,23 @@ def main():
         logging.info("You decide to clear path before work")
         clear_files_in_path(parsed_args.path_to_save_files, parsed_args.file_name)
 
+    if parsed_args.clear_files_in_path():
+        logging.info("Cleared data before generating files")
+
+    if int(parsed_args.multiprocessing) <= 0:
+        logging.error(f"You chose {parsed_args.multiprocessing} number of processes used to create files. Can't be less than 0")
+
     if parsed_args.path_to_save_files is None and parsed_args.files_count is not None:
         logging.info("You didn't choose the path dir, so path is current file ")
 
     if int(parsed_args.files_count) > 0 and (parsed_args.file_name, parsed_args.file_prefix, parsed_args.path_to_save_files, parsed_args.data_lines, parsed_args.data_schema) is not None:
         logging.debug(f'The number of processes used to create files is {parsed_args.multiprocessing}')
         files = construct_files(parsed_args.file_name, parsed_args.file_prefix, parsed_args.files_count)
-
-        with ThreadPoolExecutor(max_workers=int(parsed_args.multiprocessing)) as executor:
+        workers_number = int(parsed_args.multiprocessing)
+        if workers_number > number_of_processes:
+            logging.info(f"Value of multiprocessing is higher than CPUs in the system. max CPU {number_of_processes} used instead")
+            workers_number = number_of_processes
+        with ThreadPoolExecutor(max_workers=workers_number) as executor:
             futures = [executor.submit(create_output_file, parsed_args.data_lines, parsed_args.data_schema, parsed_args.path_to_save_files, file) for file in files]
         logging.info(f"All files created. Created {len(futures)} files")
 
